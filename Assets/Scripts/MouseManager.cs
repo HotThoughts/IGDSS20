94b2e845d54f653078230cce496a597c4d2f5bdc
AliveIT <aliveit.elnur@gmail.com>
2020-05-16 15:48:32 +0400

    Assignment2 done (for sure)

--- Assets/Scripts/MouseManager.cs
+++ Assets/Scripts/MouseManager.cs
@@ -4,18 +4,19 @@ using UnityEngine;
 
 public class MouseManager : MonoBehaviour
 {
-    public float scrollSpeed = 10f;
+    public float scrollSpeed = 100f;
     private float maxY = 85f;
     private float minY = 25f;
     public float panSpeed;
     public Bounds bounds;
     public Camera cam;
     private Vector3 prev_pos;
+    private Vector3 smoothCamPos;
     // Start is called before the first frame update
     void Start()
     {
         cam = Camera.main;
-
+        smoothCamPos = cam.transform.position; // Initial move
         // Determine scene bounds
         var rnds = FindObjectsOfType<Renderer>();
         if (rnds.Length == 0)
@@ -44,6 +45,7 @@ public class MouseManager : MonoBehaviour
             pos.z = Mathf.Clamp(pos.z, bounds.min.z, bounds.max.z);
 
             cam.transform.position = pos;//Vector3.Lerp(prev_pos, pos, Time.deltaTime);
+            smoothCamPos = pos;
         }
             
         // left button
@@ -53,20 +55,27 @@ public class MouseManager : MonoBehaviour
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit))
                 print(hit.collider.gameObject.name);
-                
         }
         if(Input.GetAxis("Mouse ScrollWheel") != 0f){
             // zoom in and out
             Vector3 pos = cam.transform.position;
             var scrollAxis = Input.GetAxis("Mouse ScrollWheel");
+            // Condition for fixing change of X axis while Y is max / min
+            if (pos.y <= maxY - 5f && pos.y >= minY + 5f)
+            {
+                pos.x -= scrollSpeed * scrollAxis;
+                pos.x = Mathf.Clamp(pos.x, bounds.min.x, bounds.max.x);
+            }
             pos.y -= scrollSpeed * scrollAxis;
-            pos.x -= scrollSpeed * scrollAxis;
             pos.y = Mathf.Clamp(pos.y, minY, maxY);
-            pos.x = Mathf.Clamp(pos.x, bounds.min.x, bounds.max.x);
-            cam.transform.position = pos;
-            // limits
-            //cam.transform.position = Mathf.Clamp(cam.fieldOfView, minY, maxY);
-
+            this.smoothCamPos = pos;
         }
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 desiredPos = this.smoothCamPos;
+        Vector3 smoothedPos = Vector3.Lerp(cam.transform.position, desiredPos, 0.125f);
+        cam.transform.position = smoothedPos;
     }
 }
